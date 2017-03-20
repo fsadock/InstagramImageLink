f82d4e6d135d47651e40dfed39477111d1cafb70
Felipe Sadock <fsadock@gmail.com>
2017-03-19 23:35:41 -0300

    Update getInstPic.py

--- getInstPic.py
+++ getInstPic.py
@@ -3,21 +3,21 @@ import urllib.request
 import requests
 import os, platform
 
-#url = input("\n\nInsert the instagram URL: ")
-url = "https://www.instagram.com/p/BOngHOGDD28Dmrt0tevugnY1w779VaRprwMvDI0/"
+url = input("\n\nInsert the instagram URL: ")
 
-pagina = requests.get(url)
-content = html.fromstring(pagina.text)
+page = requests.get(url)
+content = html.fromstring(page.text)
 
+#get the proper link
 pic = content.xpath('//meta[@property="og:image"]/@content')
 pic = pic[0]
-#print(pic)
 
 if platform.system() == 'Windows':
 	urllib.request.urlretrieve(pic, os.path.join(os.environ["HOMEPATH"], "Desktop\\foto.jpg"))
 if platform.system() == 'Mac':
 	urllib.request.urlretrieve(pic, os.path.join(os.path.expanduser('~'), 'Desktop'))
 
+#opens the picture on the browser
 os.startfile(pic)
 
-print("\n\nFOTO SALVA NA AREA DE TRABALHO!!!")
+print("\n\nSAVED ON DESKTOP!!!")
